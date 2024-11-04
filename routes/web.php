5e789fb1a836a7fea09a0620b7aadafd817ba6f8
enomarozi <marozieno0@gmail.com>
2024-11-04 10:43:51 +0700

    04-11-2024

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,4 @@ Route::GET('/',[AccountsController::class,'index'])->name("index");
 
 Route::GET('/login',[AccountsController::class,'login'])->name("login");
 Route::POST('/loginAction',[AccountsController::class,'loginAction'])->name("loginAction");
+Route::GET('/logout',[AccountsController::class,'logout'])->name("logout");
