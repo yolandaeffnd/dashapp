14eb2c96f86fb47e9249af5e993cfebd86b551a9
Fathonahghaniyyah <fathonahghaniyyahcr@gmail.com>
2025-03-10 15:07:52 +0700

    #ikuII

---
+++ app/Http/Controllers/renstraChartController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\File;
+class renstraChartController extends Controller
+{
+    //
+    public function chartikuII(Request $request){
+        $response = Http::get('http://127.0.0.1:8000/api/mhs/data-iku-II');
+        $data = $response->json()['data'];
+    dd($data);
+    $fakultas = collect($data)->pluck('fakultas_nama')->unique();
+    }
+}
